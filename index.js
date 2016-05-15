d0cedb466aa8281bfd53814db78b9dfd56cc8fad
andujara <andujar.anna@gmail.com>
2016-05-15 11:50:49 +0200

    adds error pages and adds sections to the main layout.

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express-handlebars') .create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars') .create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -18,23 +27,18 @@ app.get('/', function(req, res){
     res.render('cover');
 });
 
+/*********************************** ERROR HANDLERS **************************************/
 // custom 404 page
 app.use(function(req, res){
-	res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+	res.render('404');
 });
 
 // custom 500 page
-app.use(function(err, req, res, next){ console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
+app.use(function(err, req, res, next){
+    res.render('500');
 });
 
-/************************************* END ROUTES *****************************************/
-
-// start app
+/************************************** START APP ****************************************/
 app.listen(app.get('port'), function(){
 	console.log( 'Started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });
